ff3509f826d1d969d376da95d1c245de93fc9d5a
mokoshin0720 <shinya.work0720@gmail.com>
2021-07-26 19:31:31 +0900

    [add]todo app

--- todo/lib/main.dart
+++ todo/lib/main.dart
@@ -16,23 +16,43 @@ class MyTodoApp extends StatelessWidget {
   }
 }
 
+class TodoListPage extends StatefulWidget {
+  @override
+  _TodoListPageState createState() => _TodoListPageState();
+}
+
 // MyTodoAppから呼び出される
-class TodoListPage extends StatelessWidget {
+class _TodoListPageState extends State<TodoListPage> {
+  List<String> todoList = [];
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text("TODO"),
+        title: Text("リスト一覧ページ"),
       ),
-      body: Center(
-        child: Text('リスト一覧'),
-      ),
+      body: ListView.builder(
+          itemCount: todoList.length,
+          itemBuilder: (context, index) {
+            return Card(
+              child: ListTile(
+                title: Text(todoList[index]),
+              ),
+            );
+          }),
       floatingActionButton: FloatingActionButton(
-        onPressed: () {
+        onPressed: () async {
           // "push"でTodoAddPageに遷移
-          Navigator.of(context).push(MaterialPageRoute(builder: (context) {
-            return TodoAddPage();
-          }));
+          final newListText = await Navigator.of(context).push(
+            MaterialPageRoute(builder: (context) {
+              return TodoAddPage();
+            }),
+          );
+          if (newListText != null) {
+            setState(() {
+              todoList.add(newListText);
+            });
+          }
         },
         child: Icon(Icons.add),
       ),
@@ -41,18 +61,55 @@ class TodoListPage extends StatelessWidget {
 }
 
 // TodoListPageから呼び出される
-class TodoAddPage extends StatelessWidget {
+class _TodoAddPageState extends State<TodoAddPage> {
+  String _text = ''; // 入力されたテキストをデータとして持つ
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Center(
-        child: TextButton(
-          onPressed: () {
-            Navigator.of(context).pop();
-          },
-          child: Text("リスト追加画面（クリックで戻る）"),
+      appBar: AppBar(
+        title: Text('リスト追加ページ'),
+      ),
+      body: Container(
+        padding: EdgeInsets.all(64),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            TextField(
+              onChanged: (String value) {
+                setState(() {
+                  _text = value;
+                });
+              },
+            ),
+            const SizedBox(height: 8),
+            Container(
+              width: double.infinity, // 横幅いっぱい
+              child: ElevatedButton(
+                onPressed: () {
+                  Navigator.of(context).pop(_text); // 引数に指定することで、前の画面にデータを渡す
+                },
+                child: Text('リスト追加', style: TextStyle(color: Colors.white)),
+              ),
+            ),
+            const SizedBox(height: 8),
+            Container(
+              width: double.infinity,
+              child: TextButton(
+                onPressed: () {
+                  Navigator.of(context).pop();
+                },
+                child: Text("キャンセル"),
+              ),
+            )
+          ],
         ),
       ),
     );
   }
+}
+
+class TodoAddPage extends StatefulWidget {
+  @override
+  _TodoAddPageState createState() => _TodoAddPageState();
 }
